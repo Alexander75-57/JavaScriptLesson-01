f30df8db27090faadf71916dc66e1128082f7dd5
Oleksandr M <ace.7557@yahoo.com>
2023-10-12 16:27:28 +0300

    Lesson_23_Tasks

--- js/script.js
+++ js/script.js
@@ -34,7 +34,36 @@ first: for (let i = 0; i < 3; i++) {
 		console.log(`Second Level: ${j}`);
 		for (let k = 0; k < 3; k++) {
 			if (k === 2) continue first;
-			console.log(`Second Level: ${k}`);
+			console.log(`Third Level: ${k}`);
 		}
+	}
+}
+// Tasks
+// 1
+let num = 5;
+while (num <= 10) {
+	console.log(num);
+	num++;
+}
+
+//2
+for (let i = 20; i >= 10; i--) {
+	if (i === 13) break;
+	console.log(i);
+}
+
+//3
+for (let i = 2; i <= 10; i++) {
+	if (i % 2 === 0)
+	console.log(i);
+}
+
+//4
+
+for (let i = 2; i <= 16; i++) {
+	if (i % 2 === 0) {
+		continue;
+	} else {
+		console.log(i);
 	}
 }
