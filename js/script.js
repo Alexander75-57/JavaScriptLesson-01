5c01a50bb61a76a15d7c73d11613515e565368fa
Oleksandr M <ace.7557@yahoo.com>
2023-10-13 14:08:25 +0300

    Lesson_23_Tasks

--- js/script.js
+++ js/script.js
@@ -76,3 +76,13 @@ while (num < 16) {
 	}	
 	
 }
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+	for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
+}
