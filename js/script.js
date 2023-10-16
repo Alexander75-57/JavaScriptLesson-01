e9b695782e465d98202ad1f9266d7231d48b4a61
Oleksandr75 <ace.7557@yahoo.com>
2023-10-16 19:53:15 +0300

    Lesson_23-3

--- js/script.js
+++ js/script.js
@@ -111,17 +111,36 @@ function secondTask() {
     const data = [5, 10, 'Shopping', 20, 'Homework'];
 
     // Пишем решение вот тут
-    
-	for (let i = 0; i <= arr.length; i++) {
-		if data[i] === Number  {
-			data[i] = (arr[i]*2);
+
+	for (let i = 0; i < data.length; i++) {
+		
+		if (typeof(data[i]) === 'number') {
+			data[i] = (data[i]*2);
 		}
 		else {
-			data[i] = 'arr[i] - done';
-		} 
+			data[i] === 'string'; 
+			data[i] = (data[i] + ' - done'); 
+			} 
+	}
+	console.log(data);
+    
+    // Не трогаем
+    return data;
+}
+
+// Место для третьей задачи
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    // Пишем решение вот тут
+    
+	for (let i = 0; i < data.length; i++) {
+		result[i] = data[(data.length - 1) - i];
 	}
 	console.log(result);
     
     // Не трогаем
-    return data;
+    return result;
 }
