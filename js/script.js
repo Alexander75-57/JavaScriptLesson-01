e73771f0f03bd274a16a6ab968d8df525e576593
Oleksandr M <ace.7557@yahoo.com>
2023-10-16 14:39:57 +0300

    Lesson_23-2_Tasks

--- js/script.js
+++ js/script.js
@@ -86,3 +86,42 @@ function fifthTask() {
     console.log(arrayOfNumbers);
     return arrayOfNumbers;
 }
+
+// Место для первой задачи
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    // Пишем решение вот тут
+ 	
+	for (let i = 0; i < arr.length; i++) {
+		result[i] = arr[i];
+	}
+	console.log(result);
+	    
+    // Не трогаем
+    return result;
+}
+
+
+// Место для второй задачи
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    // Пишем решение вот тут
+    
+	for (let i = 0; i <= arr.length; i++) {
+		if data[i] === Number  {
+			data[i] = (arr[i]*2);
+		}
+		else {
+			data[i] = 'arr[i] - done';
+		} 
+	}
+	console.log(result);
+    
+    // Не трогаем
+    return data;
+}
