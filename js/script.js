74f88aa55117f66460d242a9a81a420b08ef4abb
Oleksandr M <ace.7557@yahoo.com>
2023-11-01 11:54:20 +0200

    Lesson_27

--- js/script.js
+++ js/script.js
@@ -71,4 +71,20 @@ promotion(res);
 
 // ---------------------------------
 
+function test() {
+	for (let i=0; i<5; i++) {
+		console.log(i);
+		if (i === 3) return;
+	}
+	console.log('Done');
+}
+
+test();
+
+//-------------------------------------
+
+function doNothing() {};
+console.log(doNothing() === undefined);
+
+//------------------------------------
 
