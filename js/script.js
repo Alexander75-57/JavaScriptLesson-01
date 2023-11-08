a5904253c3428f3c6f597d8421e5ee90ad3e721e
Oleksandr M <ace.7557@yahoo.com>
2023-11-08 16:06:38 +0200

    Lesson_28

--- js/script.js
+++ js/script.js
@@ -134,3 +134,12 @@ console.log(arr.length);
 console.log(arr[1]);
 
 //-----------------------------------
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
+//-----------------------------------
